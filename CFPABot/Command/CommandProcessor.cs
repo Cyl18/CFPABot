a460765d6623d831452cbebc493723aa6b887fdf
Cyl18 <cyl18a@gmail.com>
2023-08-17 20:47:16 +0800

    allow-empty

--- CFPABot/Command/CommandProcessor.cs
+++ CFPABot/Command/CommandProcessor.cs
@@ -176,23 +176,13 @@ namespace CFPABot.Command
                             sb.AppendLine("无法获取到指定用户。");
                             continue;
                         }
+                        var r = GetRepo();
 
-                        var email = $"Co-authored-by: {coAuthor.Login} <{coAuthor.Id}+{coAuthor.Login}@users.noreply.github.com>";
-                        var prBody = pr.Body;
-                        if (prBody.IsNullOrWhiteSpace())
-                        {
-                            prBody = email;
-                            goto end;
-                        }
-                        if (!prBody.EndsWith('\n') || !(prBody.Split('\n').Last().Split(':').FirstOrDefault().Equals("Co-authored-by", StringComparison.OrdinalIgnoreCase)))
-                        {
-                            prBody += '\n';
-                        }
+                        r.Run("commit --allow-empty -m \"" +
+                                       $"别看我，我只是个协作\n\n" +
+                                       $"Co-authored-by: {coAuthor.Login} <{coAuthor.Id}+{coAuthor.Login}@users.noreply.github.com>" +
+                                       "\"");
 
-                        prBody += email;
-                        end:
-                        await GitHub.Instance.PullRequest.Update(Constants.RepoID, prid,
-                            new PullRequestUpdate() {Body = prBody});
                         sb.AppendLine("完成。");
                     }
 
